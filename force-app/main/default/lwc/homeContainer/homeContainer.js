803ffded6c476f3ea255c19722f45b238f33734a
maksim grishchenko <maksim.grishchenko@vrpconsulting.com>
2022-05-03 20:15:04 +0300

    Home page components mock-up improvements.

--- force-app/main/default/lwc/homeContainer/homeContainer.js
+++ force-app/main/default/lwc/homeContainer/homeContainer.js
@@ -6,19 +6,19 @@ export default class HomeContainer extends NavigationMixin(LightningElement) {
     get items() {
         return [
             {
-                title: 'Stock',
+                title: 'Stocks & Quotes',
                 description: 'Get the latest stock information & quotes, data analysis, as well as a general overview of the market landscape from Nasdaq.',
                 svg: 'trending',
                 url: '/stocks'
             },
             {
-                title: 'News',
+                title: 'News & Tips',
                 description: 'Find the up-to-date breaking news and information on the top business, investments, politics, and more.',
                 svg: 'news',
                 url: '/news'
             },
             {
-                title: 'Tools',
+                title: 'Calculators & Tools',
                 description: 'Use technical analysis tools that can help you achieve your financial goals.',
                 svg: 'custom_apps',
                 url: '/tools'
